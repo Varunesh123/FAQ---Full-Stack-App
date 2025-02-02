a55e85fd555d056a8e85cba5c8dcabb3e7b2d4f4
Varunesh Pathak <varuneshpathak21@gmail.com>
2025-02-02 17:14:05 +0530

    fix: env var for client axios

--- faq-admin/src/api/faqApi.js
+++ faq-admin/src/api/faqApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const Axios = axios.create({
-  baseURL: 'http://localhost:3000'
+  baseURL: import.meta.env.VITE_SERVER_URI || 'http://localhost:3000'
 })
 
 export default Axios
